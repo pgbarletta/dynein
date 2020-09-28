725366aab8e9f4a826c020393835b273ab400f0c
pgbarletta <pbarletta@gmail.com>
2020-09-28 09:37:25 -0300

    LBM7 ?_pdt run data

--- run/lbm7/4_pdt/pdt.sh
+++ run/lbm7/4_pdt/pdt.sh
@@ -9,13 +9,13 @@ do
     mdin=pdt.in
     mdout=${i}pdt.out
     prmtop=/home/almacen/pbarletta/dynein/top_files/lbm7.prmtop
-    inpcrd=${k}p3_lbm7.rst7
-    restrt=${i}p3_lbm7.rst7
-    mdcrd=${i}p3_lbm7.nc
+    inpcrd=${k}p4_lbm7.rst7
+    restrt=${i}p4_lbm7.rst7
+    mdcrd=${i}p4_lbm7.nc
 
     if [ $i == 0 ]
     then
-        inpcrd=3_lbm7.rst7
+        inpcrd=4_lbm7.rst7
         mdin=pre_pdt.in
 
         pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
