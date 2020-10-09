f36349b389b9b6e553b50d834793116ccae5ef3a
pgbarletta <pbarletta@gmail.com>
2020-10-09 09:52:58 -0300

    1F95 ?_pdt data

---
+++ run/1f95/2_pdt/pdt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="0"
+
+for i in `cat step.list`
+do
+    echo "Step:" $i
+    k=`expr $i - 1`
+
+    mdin=pdt.in
+    mdout=${i}pdt.out
+    prmtop=/home/almacen/pbarletta/dynein/top_files/1f95.prmtop
+    inpcrd=${k}p2_1f95.rst7
+    restrt=${i}p2_1f95.rst7
+    mdcrd=${i}p2_1f95.nc
+
+    if [ $i == 0 ]
+    then
+        inpcrd=2_1f95.rst7
+        mdin=pre_pdt.in
+
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    else
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    fi
+done
