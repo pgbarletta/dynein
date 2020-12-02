8073e3c3d295f125ff6ef981f069ec11d598313b
pgbarletta <pbarletta@gmail.com>
2020-12-02 09:49:02 -0300

    Redoing 1F96 for the 2nd time. mhe data and mhe analysis

---
+++ run/1f96/mhe/mhe_pre_filer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pdb=1f95
+
+mkdir -p m/outputs
+mkdir -p h/outputs
+mkdir -p e/outputs
+
+cp ../../${pdb}/mhe/m/outputs/*sh m/outputs
+
+cp ../../${pdb}/mhe/h/outputs/mdout.sh h/outputs/
+cp ../../${pdb}/mhe/h/outputs/process_mdout.perl h/outputs/
+
+cp ../../${pdb}/mhe/e/outputs/mdout.sh e/outputs
+cp ../../${pdb}/mhe/e/outputs/process_mdout.perl e/outputs/
+
+exit 0
