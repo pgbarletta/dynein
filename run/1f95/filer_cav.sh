6e47d01d8d1200d114336df0b67d6690e4936642
pgbarletta <pbarletta@gmail.com>
2020-12-03 12:13:41 -0300

    1F95 meh

---
+++ run/1f95/filer_cav.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+pdb=1f95
+
+#
+# Despues hay q arreglar ${i}_top_${pdb} agregandoles letras de cadena
+# y generer het_${i}_top_${pdb}
+#
+
+for i in {1..4}
+do
+    mkdir ${i}_cavidad
+    cd ${i}_cavidad
+
+    cp ../cavidad/*cfg .
+    cp ../cavidad/*sh .
+    cp ../${i}_pca/${i}_top_${pdb}.pdb .
+
+    sed -i s/top_/${i}_top_/g get_cavities.sh
+    sed -i s/top_/${i}_top_/g get_ch.sh
+    sed -i s/top_/${i}_top_/g get_trj.sh
+    sed -i s/full_/${i}_full_/g get_trj.sh
+    sed -i s/pca/${i}_pca/g get_trj.sh
+    
+    sed -i s/vol_acb_/${i}_vol_acb_/g get_trj.sh
+    sed -i s/vol_adb_/${i}_vol_adb_/g get_trj.sh
+    sed -i s/vol_ecf_/${i}_vol_ecf_/g get_trj.sh
+    sed -i s/vol_edf_/${i}_vol_edf_/g get_trj.sh
+
+    sed -i s/vol_acb_/${i}_vol_acb_/g get_cavities.sh
+    sed -i s/vol_adb_/${i}_vol_adb_/g get_cavities.sh
+    sed -i s/vol_ecf_/${i}_vol_ecf_/g get_cavities.sh
+    sed -i s/vol_edf_/${i}_vol_edf_/g get_cavities.sh
+
+    cd ..
+done
---
+++ run/1f95/min_rmsd.jl
@@ -0,0 +1,36 @@
+# Script p/ encontrar y escribir el PDB "top", es decir, el frame más parecido
+# al average de la corrida.
+# Leo el archivo de rmsd entre el PDB average y la corrida (fiteada a ese
+# mismo average) y encuentro el frame correspondiente. Uso ese nro de frame p/
+# modificar el archivo plantilla "get_top_cpp" y lo corro con cpptraj p/ obtener el PDB.
+
+using DelimitedFiles
+
+pdb = "1f95"
+casa = joinpath("/home/pbarletta/labo/20/dynein/run", pdb)
+cd(casa)
+cpp_plantilla = readlines(joinpath("plantillas", "get_top_cpp"))
+
+for idx = 1:4
+    cpp = copy(cpp_plantilla)
+    rmsd_fn = joinpath(string(idx, "_pca"), string(idx, "_", "rmsd_avg"))
+
+    rms = convert(Array{Float64, 1}, readdlm(rmsd_fn)[2:end, 2])
+    frm = findmin(rms)[2]
+
+    cpp[1] = replace(cpp[1], "apo" => pdb)
+
+    cpp[3] = replace(cpp[3], "1_full" => string(idx, "_full"))
+    cpp[3] = replace(cpp[3], "X" => string(frm))
+    cpp[3] = replace(cpp[3], "apo" => pdb)
+
+    cpp[4] = replace(cpp[4], "apo" => pdb)
+    cpp[4] = replace(cpp[4], "1_top" => string(idx, "_top"))
+    
+    writedlm(joinpath(string(idx, "_pca"), "get_top_cpp"), cpp)
+
+    cd(string(idx, "_pca"))
+    run(`cpptraj -i get_top_cpp`)
+    cd("..")
+end
+
