ddce86ba2e37994532a642e56940875405533b38
pgbarletta <pbarletta@gmail.com>
2020-12-15 21:47:39 -0300

    Fixed LF3F cavity

--- run/lf3f/cavidad/get_ch.sh
+++ run/lf3f/cavidad/get_ch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ANA2 top_lf3f.pdb -c acb.cfg -t ch_acb
-ANA2 top_lf3f.pdb -c adb.cfg -t ch_adb
+ANA2 het_top_lf3f.pdb -c acb.cfg -t ch_acb
+ANA2 het_top_lf3f.pdb -c adb.cfg -t ch_adb
 
 exit 0
