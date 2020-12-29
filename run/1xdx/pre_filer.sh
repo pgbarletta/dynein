f77d0edd21e6e985d101f95cc70a0807bd43874d
pgbarletta <pbarletta@gmail.com>
2020-12-29 14:30:56 -0300

    1XDX meh

---
+++ run/1xdx/pre_filer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pdb1=1f95
+pdb2=1xdx
+range1=188
+range2=228
+
+mkdir pca
+mkdir cavidad
+
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/pca/fit_avg_cpp > pca/fit_avg_cpp
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/pca/get_top_cpp > pca/get_top_cpp
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/pca/pca_cpp > pca/pca_cpp
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/pca/rms_cpp > pca/rms_cpp
+
+sed -i s/${range1}/${range2}/g pca/fit_avg_cpp
+sed -i s/${range1}/${range2}/g pca/get_top_cpp
+sed -i s/${range1}/${range2}/g pca/pca_cpp
+sed -i s/${range1}/${range2}/g pca/rms_cpp
+
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/cavidad/get_ch.sh > cavidad/get_ch.sh
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/cavidad/get_cavities.sh > cavidad/get_cavities.sh
+sed s/${pdb1}/${pdb2}/g ../${pdb1}/cavidad/get_trj.sh > cavidad/get_trj.sh
+chmod +x ./cavidad/*sh
+cp ../${pdb1}/cavidad/*cfg ./cavidad/
+
+cd pca
+cpptraj < pca_cpp
+cpptraj < fit_avg_cpp
+cpptraj < rms_cpp 
+cd ..
+
+exit 0
