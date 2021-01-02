dbe4b5fa402966f66c633686e08eee82f9aaeab4
pgbarletta <pbarletta@gmail.com>
2021-01-02 11:29:04 -0300

    1XDX 4_pdt data

---
+++ run/1xdx/4_pdt/pdt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="0"
+
+for i in `cat step.list`
+do
+    echo "Step:" $i
+    k=`expr $i - 1`
+
+    mdin=pdt.in
+    mdout=${i}pdt.out
+    prmtop=/home/almacen/pbarletta/dynein/top_files/1xdx.prmtop
+    inpcrd=${k}p4_1xdx.rst7
+    restrt=${i}p4_1xdx.rst7
+    mdcrd=${i}p4_1xdx.nc
+
+    if [ $i == 0 ]
+    then
+        inpcrd=4_1xdx.rst7
+        mdin=pre_pdt.in
+
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    else
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    fi
+done
