28c091bb0fe9ac0d6261a29117009cb95f9232b4
pgbarletta <pbarletta@gmail.com>
2021-01-27 09:30:28 -0300

    3ZKF main run pdt data

---
+++ run/3zkf/pdt/pdt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="0"
+
+for i in `cat step.list`
+do
+    echo "Step:" $i
+    k=`expr $i - 1`
+
+    mdin=pdt.in
+    mdout=${i}pdt.out
+    prmtop=/home/almacen/pbarletta/dynein/top_files/3zkf.prmtop
+    inpcrd=${k}p3zkf.rst7
+    restrt=${i}p3zkf.rst7
+    mdcrd=${i}p3zkf.nc
+
+    if [ $i == 1 ]
+    then
+        inpcrd=../mhe/e/75e3zkf.rst7
+
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    else
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    fi
+done
