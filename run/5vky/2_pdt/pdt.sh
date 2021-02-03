cb859356cc48b75935ca8bf3da5b89f2d615f6a1
pgbarletta <pbarletta@gmail.com>
2021-02-03 10:49:34 -0300

    5VKY 2 pdt data

---
+++ run/5vky/2_pdt/pdt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="0"
+
+for i in `cat step.list`
+do
+    echo "Step:" $i
+    k=`expr $i - 1`
+
+    mdin=pdt.in
+    mdout=${i}pdt.out
+    prmtop=/home/almacen/pbarletta/dynein/top_files/5vky.prmtop
+    inpcrd=${k}p2_5vky.rst7
+    restrt=${i}p2_5vky.rst7
+    mdcrd=${i}p2_5vky.nc
+
+    if [ $i == 0 ]
+    then
+        inpcrd=2_5vky.rst7
+        mdin=pre_pdt.in
+
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    else
+        pmemd.cuda -O -i $mdin -o $mdout -p $prmtop -c $inpcrd -r $restrt -x $mdcrd
+        echo "Done step:" $i
+    fi
+done
